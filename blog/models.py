f60778486c459f5e889ce35c21b0477b596875fa
Alan <alanjameschapman@gmail.com>
2024-02-08 17:52:24 +0000

    Add Comment model and remove challenge field from Comment model

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Title: {self.title} | Author: {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -25,3 +31,8 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment: {self.body} by {self.author}"
