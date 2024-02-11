960599f4920b3a5c83de7b1dc006b7152380b6e3
Alan <alanjameschapman@gmail.com>
2024-02-11 11:29:30 +0000

    Fix navigation link and display post excerpt in index.html

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,6 @@ from .models import Post
 # from django.http import HttpResponse
 
 # Create your views here.
-# def my_blog(request):
-#     return HttpResponse("Hello, Blog!")
 
 
 class PostList(generic.ListView):
