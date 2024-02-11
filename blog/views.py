94e285375739c13cd795513daaf2cf5849d0e316
Alan <alanjameschapman@gmail.com>
2024-02-11 17:54:24 +0000

    Fix bug in login functionality

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+# from django.http import HttpResponse
+
+# Create your views here.
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.all().order_by("-created_on")
+    template_name = "blog/index.html"
+    paginate_by = 6
